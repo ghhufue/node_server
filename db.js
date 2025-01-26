184bee832f35ec386115ab19985b964c4f3e51e3
yht <yht20050302@sjtu.edu.cn>
2025-01-26 13:57:39 +0800

    modify the database path

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "remote_user",
-    password: "yht20050302",
-    database: "chatappdb",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-  });
+  host: "chatappdb.yht20050302.top",
+  port: 53578,
+  user: "remote_user",
+  password: "yht20050302",
+  database: "chatappdb",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 module.exports = pool;
